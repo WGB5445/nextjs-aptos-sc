deea716c573cb1331c5f8c39844ed697baa51e91
WGB5445 <919603023@qq.com>
2025-01-17 04:23:16 +0800

    添加 GitHub Pages 部署配置，更新 next.config.ts 和 package.json

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: '/nextjs-aptos-sc',  // 设置你的 GitHub 仓库名称
+  assetPrefix: '/nextjs-aptos-sc',  // 同上
+  trailingSlash: true,
 };
 
 export default nextConfig;
